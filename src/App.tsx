a470e44d265990214ef375749541e00afbb23b3c
Winnie-Udel <winnie@udel.edu>
2025-03-18 18:22:08 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Names: Winnie Li</p>
         <a
           className="App-link"
           href="https://reactjs.org"
