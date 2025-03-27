a7cf7e60f252924021f75041d6f00b8504e54c85
Winnie-Udel <winnie@udel.edu>
2025-03-27 01:14:18 -0400

    Reformat

--- src/interfaces/question.ts
+++ src/interfaces/question.ts
@@ -1,20 +1,16 @@
 /** QuestionType influences how a question is asked and what kinds of answers are possible */
-export type QuestionType =
-  | "multiple_choice_question"
-  | "checklist_question"
-  | "binary_question"
-  | "opinion_question";
+export type QuestionType = "multiple_choice_question" | "checklist_question" | "binary_question" | "opinion_question";
 
 /** A representation of a Question in a quizzing application */
 export interface Question {
-  /** A unique identifier for the question */
-  id: number;
-  /** The human-friendly title of the question */
-  name: string;
-  /** The instructions and content of the Question */
-  body: string;
-  /** The kind of Question; influences how the user answers and what options are displayed */
-  type: QuestionType;
-  /** The possible answers for a Question */
-  options: string[] | string[][];
+    /** A unique identifier for the question */
+    id: number;
+    /** The human-friendly title of the question */
+    name: string;
+    /** The instructions and content of the Question */
+    body: string;
+    /** The kind of Question; influences how the user answers and what options are displayed */
+    type: QuestionType;
+    /** The possible answers for a Question */
+    options: string[] | string[][];
 }
