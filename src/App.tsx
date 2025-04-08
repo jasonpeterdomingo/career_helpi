2bafe5ce1a3a9db24725b70d7a6504861e96224a
Jason Domingo <jayprdom@udel.edu>
2025-04-08 15:30:50 -0400

    refactored api code by creating footer component and api hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Form } from "react-bootstrap";
 import { NavigationBar } from "./components/NavigationBar";
 import { RenderPage } from "./components/RenderPage";
+import { Footer } from "./components/Footer";
 import { FontSizeAdjuster } from "./components/FontSizeAdjuster";
 import { PAGE } from "./types/page";
+import { useApiKey } from "./hooks/useApiKey";
 
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
+// //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 
-let keyData = "";
-/*
-const saveKeyData = "MYKEY";
-const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
-if (prevKey !== null) {
-  keyData = JSON.parse(prevKey);
-}
-  */
+// let keyData = "";
+// const saveKeyData = "MYKEY";
+// const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+// if (prevKey !== null) {
+//   keyData = JSON.parse(prevKey);
+// }
 
 function App() {
-  const [key, setKey] = useState<string>(keyData); //for api key input
+  const { key, setKey } = useApiKey();
+  // const [key, setKey] = useState<string>(keyData); //for api key input
   const [fontSize, setFontSize] = useState<number>(16); // For adjusting font size
   const [currentPage, setPage] = useState<PAGE>("home"); // For rendering different pages
 
-  //sets the local storage item to the api key the user inputed
-  /*
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-  }
-    */
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-    setKey(event.target.value);
-  }
+  // //sets the local storage item to the api key the user inputed
+  // function handleSubmit() {
+  //   localStorage.setItem(saveKeyData, JSON.stringify(key));
+  //   window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+  // }
+  // //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  // function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setKey(event.target.value);
+  // }
 
   return (
     <div className="App">
@@ -43,22 +41,7 @@ function App() {
       <FontSizeAdjuster setFontSize={setFontSize} />
       <hr />
       <RenderPage setPage={setPage} page={currentPage} fontSize={fontSize} />
-      {/* Footer with API Key Input */}
-      <footer className="App-footer">
-        <Form>
-          <Form.Label>API Key:</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder={key ? "" : "Insert API Key Here"} // originally just "Insert API Key Here", but i had to use the key variable so there were no deploy errors
-            onChange={changeKey}
-          ></Form.Control>
-          <br></br>
-          {/* <Button className="Submit-Button" onClick={handleSubmit}>
-            Submit
-          </Button> */}
-        </Form>
-        <p>Names: Winnie Li, Jason Domingo, Ember Kerstetter</p>
-      </footer>
+      <Footer apiKey={key} onKeyChange={(e) => setKey(e.target.value)}></Footer>
     </div>
   );
 }
