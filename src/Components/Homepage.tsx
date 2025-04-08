8cd95fd34cba5c17ec8325d27d710d27100126e1
Ember Kerstetter <emberk@udel.edu>
2025-04-08 14:45:39 -0400

    Shortened the descriptions a bit, and made them cards. Formatting is yet to be done

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import "./Components.css";
 
 type Page = "home" | "basic" | "detailed";
@@ -22,27 +22,38 @@ export function Homepage({ navigatePage, fontSize }: HomepageProps) {
     <div style={{ fontSize: `${fontSize}px` }}>
       <div className="text">
         <h1 className="header">Welcome to The Career Helpi!</h1>
-        <h2 className="title">Basic Questions</h2>
-        <p className="description-text">
-          Answer a set of fundamental questions to receive broad yet insightful
-          career recommendations. The results are tailored to your interests and
-          personality, providing a variety of potential career paths that align
-          with your traits. A progress bar will track your completion, and once
-          all questions are answered, you'll unlock your results.
-        </p>
-        <Button onClick={() => navigatePage("basic")}>Start Here</Button>
-
-        <h2 className="title">Detailed Questions</h2>
-        <p className="description-text">
-          Dive deeper into your career preferences by providing more specific
-          information about your work experience and priorities. This section
-          allows you to indicate whether you have career experience, enabling
-          more tailored questions about your work preferences. Additionally, you
-          can filter career options based on factors like work-life balance,
-          salary, and creative freedom—ensuring your results go beyond
-          personality and interests to reflect your ideal work environment.
-        </p>
-        <Button onClick={() => navigatePage("detailed")}>Start Here</Button>
+        <Row xs={1} md={2}>
+          <Col>
+            <Card className="card-style">
+              <Card.Title className="title">Basic Questions</Card.Title>
+              <Card.Text className="description-text">
+                Answer a set of fundamental questions to receive broad yet
+                insightful career recommendations. The results are tailored to
+                your interests and personality, providing a variety of potential
+                career paths that align with your traits. A progress bar will
+                track your completion, and once all questions are answered,
+                you'll unlock your results.
+              </Card.Text>
+              <Button onClick={() => navigatePage("basic")}>Start Here</Button>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="card-style">
+              <Card.Title className="title">Detailed Questions</Card.Title>
+              <Card.Text className="description-text">
+                Dive deeper into your career preferences by providing more
+                specific information about your work experience and priorities.
+                Additionally, you can filter career options based on factors
+                like work-life balance, salary, and creative freedom—ensuring
+                your results go beyond personality and interests to reflect your
+                ideal work environment.
+              </Card.Text>
+              <Button onClick={() => navigatePage("detailed")}>
+                Start Here
+              </Button>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
