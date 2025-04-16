d4f66f3c997baa903ffe10e879a5ec720696d9f6
Ember Kerstetter <emberk@udel.edu>
2025-04-16 00:09:31 -0400

    set a max width apart that the hompage cards can get from one another.

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -24,27 +24,29 @@ export function Homepage({ navigatePage, fontSize }: HomepageProps) {
       <div className="content-wrapper">
         <h1 className="header">Welcome to The Career Helpi!</h1>
         <div className="homepage-wrapper">
-          <Row className="card-container">
-            <Col className="d-flex justify-content-center">
-              <div className="card-wrapper">
-                <div
-                  className="figma-card clickable-card"
-                  onClick={() => navigatePage("basic")}
-                >
-                  <div className="accent-right" />
-                  <div className="accent-left" />
-                  <div className="top-header-block" />
-                  <div className="bottom-body-block" />
-                  <div className="card-title">Basic Questions</div>
-                  <div className="card-description">
-                    Answer a set of fundamental questions to receive broad yet
-                    insightful career recommendations. The results are tailored
-                    to your interests and personality, providing a variety of
-                    potential career paths that align with your traits. A
-                    progress bar will track your completion, and once all
-                    questions are answered, you'll unlock your results.
-                  </div>
-                  {/*
+          <div className="card-row-wrapper">
+            <Row className="card-container">
+              <Col className="d-flex justify-content-center">
+                <div className="card-wrapper">
+                  <div
+                    className="figma-card clickable-card"
+                    onClick={() => navigatePage("basic")}
+                  >
+                    <div className="accent-right" />
+                    <div className="accent-left" />
+                    <div className="top-header-block" />
+                    <div className="bottom-body-block" />
+                    <div className="card-title">Basic Questions</div>
+                    <div className="card-description">
+                      Answer a set of fundamental questions to receive broad yet
+                      insightful career recommendations. The results are
+                      tailored to your interests and personality, providing a
+                      variety of potential career paths that align with your
+                      traits. A progress bar will track your completion, and
+                      once all questions are answered, you'll unlock your
+                      results.
+                    </div>
+                    {/*
                   <div
                     className="card-button"
                     onClick={() => navigatePage("basic")}
@@ -52,30 +54,31 @@ export function Homepage({ navigatePage, fontSize }: HomepageProps) {
                     <div className="card-button-text">Start Here</div>
                   </div>
       */}
-                </div>
-              </div>
-            </Col>
-
-            <Col className="d-flex justify-content-center">
-              <div className="card-wrapper">
-                <div
-                  className="figma-card clickable-card"
-                  onClick={() => navigatePage("detailed")}
-                >
-                  <div className="accent-right" />
-                  <div className="accent-left" />
-                  <div className="top-header-block" />
-                  <div className="bottom-body-block" />
-                  <div className="card-title">Detailed Questions</div>
-                  <div className="card-description">
-                    Dive deeper into your career preferences by providing more
-                    specific information about your work experience and
-                    priorities. Additionally, you can filter career options
-                    based on factors like work-life balance, salary, and
-                    creative freedom—ensuring your results go beyond personality
-                    and interests to reflect your ideal work environment.
                   </div>
-                  {/*
+                </div>
+              </Col>
+
+              <Col className="d-flex justify-content-center">
+                <div className="card-wrapper">
+                  <div
+                    className="figma-card clickable-card"
+                    onClick={() => navigatePage("detailed")}
+                  >
+                    <div className="accent-right" />
+                    <div className="accent-left" />
+                    <div className="top-header-block" />
+                    <div className="bottom-body-block" />
+                    <div className="card-title">Detailed Questions</div>
+                    <div className="card-description">
+                      Dive deeper into your career preferences by providing more
+                      specific information about your work experience and
+                      priorities. Additionally, you can filter career options
+                      based on factors like work-life balance, salary, and
+                      creative freedom—ensuring your results go beyond
+                      personality and interests to reflect your ideal work
+                      environment.
+                    </div>
+                    {/*
                 <div
                   className="card-button"
                   onClick={() => navigatePage("detailed")}
@@ -83,10 +86,11 @@ export function Homepage({ navigatePage, fontSize }: HomepageProps) {
                   <div className="card-button-text">Start Here</div>
                 </div>
                 */}
+                  </div>
                 </div>
-              </div>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
     </div>
